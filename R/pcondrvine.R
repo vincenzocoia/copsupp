dc98aad91c8f05d1a8303f2ddfb4b9a9bf02aa3a
Vincenzo Coia <vincenzo.coia@gmail.com>
2015-11-27 11:56:44 -0800

    When there are only two variables, pcondrvine just uses the pcondcop() functionality instead of the more complicated pcondD.generic(), which is more complicated and unnecessary. So, pcondD and qcondD functions should be fully deprecated now.

--- R/pcondrvine.R
+++ R/pcondrvine.R
@@ -1,8 +1,7 @@
 #' Conditional Distribution in a Regular Vine
 #'
 #' Evaluates the conditional distribution of a variable in a regular vine,
-#' given values of the other variables. WARNING: Algorithm for non-d-vine
-#' may not be working properly.
+#' given values of the other variables in that vine.
 #'
 #' @param dat vector or matrix of observations (columns are variables).
 #' @param cond Integer; the variable you wish to condition on (i.e. the
@@ -18,10 +17,8 @@
 #' @param Fmarg List of (univariate) marginal cdfs corresponding to
 #' the columns in \code{dat};
 #' each should be vectorized. Or a single function if the cdf is all the same.
-#' @param .print Logical; should the function output how it goes about
+#' @param verbose Logical; should the function output how it goes about
 #' finding the conditional distribution?
-#' @author Bo Chang wrote the function \code{rVineTruncCondCDF()} that this
-#' function heavily relies on.
 #' @details This function could do one of two things, depending on the
 #' scenario.
 #'
@@ -43,14 +40,14 @@
 #' copmat <- makeuppertri("bvncop", 2, 5)
 #' cparmat <- makeuppertri(c(1:7/10), 2, 5, byRow = FALSE)
 #' udat <- fvinesim(10, A, copmat, cparmat)
-#' pcondrvine(udat, 5, A, copmat, cparmat, .print=T)  # integrates vine density.
-#' pcondrvine(udat, 2, A, copmat, cparmat, .print=T)  # computes from D-vine formula
+#' pcondrvine(udat, 5, A, copmat, cparmat, verbose=T)  # integrates vine density.
+#' pcondrvine(udat, 2, A, copmat, cparmat, verbose=T)  # computes from D-vine formula
 #'
 #' ## C-Vine example
 #' A <- CopulaModel::Cvinearray(5)
 #' A <- truncvarray(A, 2)
 #' udat <- fvinesim(10, A, copmat, cparmat)
-#' pcondrvine(udat, 3, A, copmat, cparmat, .print=T)  # computes from general R-vine algo
+#' pcondrvine(udat, 3, A, copmat, cparmat, verbose=T)  # computes from general R-vine algo
 #'
 #' ## Array doesn't have to involve all data:
 #' A <- CopulaModel::Dvinearray(5)
@@ -67,30 +64,34 @@
 #' pcondrvine(3:5/10, 2, A, copmat, cparmat)
 #' @export
 pcondrvine <- function(dat, cond, A, copmat, cparmat, Fmarg = identity,
-                        .print = FALSE) {
+                        verbose = FALSE) {
     if (is.vector(dat)) dat <- matrix(dat, nrow = 1)
-    p <- ncol(A)
+    d <- ncol(A)
+    ptot <- ncol(dat)
     ntrunc <- nrow(A) - 1
+    vars <- varray.vars(A)
     ## Uniformize data:
     if (length(Fmarg) == 1) {
         udat <- Fmarg(dat)
     } else {
         udat <- dat
-        for (col in 1:ncol(dat)) udat[, col] <- Fmarg[[col]](dat[, col])
+        for (col in vars) udat[, col] <- Fmarg[[col]](dat[, col])
+        ## ^^ Note ^^: only the variables in the vine array get uniformized -- because
+        ##  those are the only ones that will be used, and what if no marginal
+        ##  is specified for variables outside of vars?
     }
     ## Is cond a leaf? If so, get the vine array with it as a leaf.
     Aleaf <- releaf.varray(A, leaf = cond)
     if (is.null(Aleaf)) {
-        if (.print) print(paste0("cond=", cond, " is not a leaf. ",
+        if (verbose) print(paste0("cond=", cond, " is not a leaf. ",
                                  "Obtaining conditional distribution by integration."))
-        res <- apply(dat, 1, function(row) {
-            dens_ <- function(xcond){
+        res <- apply(udat, 1, function(row) {
+            dens <- function(xcond){  # Accepts uniform variable 'cond'.
                 x <- row
                 x[cond] <- xcond
                 drvine(x, A, copmat, cparmat)
             }
-            dens <- Vectorize(dens_)
-            integrate(dens, 0, row[cond])$value / integrate(dens, 0, 1)$value
+            integrate.mv(dens, 0, row[cond]) / integrate.mv(dens, 0, 1)
         })
     } else {
         ## We'll need to re-arrange the copmat and cparmat to match Aleaf.
@@ -98,30 +99,28 @@ pcondrvine <- function(dat, cond, A, copmat, cparmat, Fmarg = identity,
         cparmat <- reform.copmat(cparmat, Aleaf, A)
         ## We'll need to re-arrange the data so that it's in order of the
         ##  vine array.
-        vars <- varray.vars(Aleaf)
-        udat <- udat[, vars]
-        if (!is.matrix(udat)) udat <- matrix(udat, ncol = length(vars))
+        revars <- varray.vars(Aleaf)
+        udat <- udat[, revars]
+        if (!is.matrix(udat)) udat <- matrix(udat, ncol = d)
         if (ncol(udat) <= 2) {
-            ## NOTE: The pcondD (and qcondD) functionality requires the copmat
-            ##  and cparmat to be in natural order. I won't bother with it,
-            ##  but since rVineTruncCondCDF() won't accept a vine with 2
-            ##  variables, and pcondD will, I'll use pcondD for that case.
-            if (.print) print(paste0("using `pcondD.generic()`. because there",
-                                     " are two variables left."))
-            res <- apply(udat, 1, function(row){
-                pcondD.generic(row, p, -(p-1),
-                               copmat = copmat, cparmat = cparmat)
-            })
+            ## NOTE: This special case is needed because
+            ##  rVineTruncCondCDF() won't accept a vine with 2 variables.
+            if (verbose) print(paste0("using `pcondcop()` because there are",
+                                     " only two variables."))
+            pcondcop <- get(paste0("pcond", copmat[1, 2]))
+            cpar <- cparmat[1, 2]
+            if (is.list(cpar)) cpar <- cpar[[1]]
+            res <- apply(udat, 1, function(row) pcondcop(row[2], row[1], cpar))
         } else {
-            if (.print) print(paste0("cond=", cond, " is a leaf. ",
+            if (verbose) print(paste0("cond=", cond, " is a leaf. ",
                                      "Using `copreg::rVineTruncCondCDF()`."))
             ## Use Bo's function
-            ## Re-label the variables in the vine so that they're 1:p, in that
+            ## Re-label the variables in the vine so that they're 1:d, in that
             ##  order.
             Aleaf <- relabel.varray(Aleaf)
-            ## Fill-in vine array so it's p x p
-            Aleaf <- rbind(Aleaf, matrix(0, nrow = p - ntrunc - 1, ncol = p))
-            diag(Aleaf) <- 1:p
+            ## Fill-in vine array so it's d x d
+            Aleaf <- rbind(Aleaf, matrix(0, nrow = d - ntrunc - 1, ncol = d))
+            diag(Aleaf) <- 1:d
             ## parvec
             # parvec <- c(t(cparmat), recursive = TRUE)
             if (is.list(cparmat[1,1])) {
