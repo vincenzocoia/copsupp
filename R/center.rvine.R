d6fcc049b95ea78cebcd2ea42750de1b00b60d4a
Vincenzo Coia <vincenzo.coia@gmail.com>
2015-12-17 21:01:42 -0500

    -Made 'center.rvine()' to replace the now-deprecated 'centervarray()'

--- R/center.rvine.R
+++ R/center.rvine.R
@@ -12,31 +12,60 @@
 #' If \code{t = ncol(A)-1}, then the entered vine isn't truncated, and the
 #' first \code{t-1} variables in the outputted array are not leaves (in fact,
 #' a natural order array is outputted, since it satisfies that requirement).
-#' @return A vine array with the same dimensions as \code{A}.
+#' @return A regular vine, with variables reordered.
+#' @examples
+#' ## Setup a vine.
+#' A <- CopulaModel::Dvinearray(5)
+#' copmat <- makeuppertri(c("gum", "mtcj", "gal", "joe",
+#'                          "frk", "gum", "bb7",
+#'                          "bb1", "indepcop",
+#'                          "bb8"), 4, 5, "")
+#' cparmat <- makeuppertri.list(c(3, 2.5, 2, 1.5,
+#'                                1, 1.3, 2, 2,
+#'                                3, 4,
+#'                                5, 0.5),
+#'                              len = c(1,1,1,1,1,1,2,2,0,2),
+#'                              4, 5)
+#' (rv <- rvine(A, copmat, cparmat,
+#'              list(pexp, identity, pnorm, pexp, sqrt)))
+#'
+#' ## Center it. Since it's a complete vine, the output is in natural order:
+#' center(rv)
+#'
+#' ## Center a truncated vine:
+#' center(trunc(rv, 2))
+#'
+#' ## It works in these cases too:
+#' center(trunc(rv, 0))
+#' center(subset(rv, 5))
+#' center(subset(rv, integer(0)))
 #' @export
 center.rvine <- function(rv) {
     A <- rv$A
     ntrunc <- nrow(A) - 1
+    ## Nothing to do if it's an independence vine:
     if (ntrunc == 0) return(rv)
     d <- ncol(A)
+    ## Nothing to do if there's less than 3 variables:
     if (d == 0 | d == 2) return(rv)
     copmat <- rv$copmat
     cparmat <- rv$cparmat
     marg <- rv$marg
+    ## If the vine is complete, just use "natural order":
     if (ntrunc == d-1) {
         Anew <- varray2NO(A)$NOa
         if (!is.null(copmat)) copmat <- reformcopmat(copmat, Anew = Anew, Aold = A)
         if (!is.null(cparmat)) cparmat <- reformcopmat(cparmat, Anew = Anew, Aold = A)
-        vA <- vars(A)
-        vnew <- vars(Anew)
+        vA <- vars(rv)
+        vnew <- vars(rvine(Anew))
         vmap <- sapply(vA, function(v_) which(vnew == v_))
         return(rvine(Anew, copmat, cparmat, rv$marg[vmap]))
     }
     ## Initiate the final array as (ntrunc+1)x(ntrunc+1) array (call it B)
     ##  using variables in A[, d], with A[d,d] going at the end.
     Bvars <- A[, d]
-    B <- rvinesubset(A, Bvars)
-    ovars <- varray.vars(A)  # Stands for "original variables"
+    B <- subset(rv, Bvars)$A
+    ovars <- vars(rv)  # Stands for "original variables"
     rvars <- setdiff(ovars, Bvars)  # Stands for "remaining variables".
     ## Convert A to a convenient form by moving labels to top row:
     Acon <- Atocon(A)
@@ -80,10 +109,16 @@ center.rvine <- function(rv) {
             B <- cbind(B, nextcol)
         }
     }
-    B
+    ## Rearrange the copula and parameter matrices, and marginals:
+    if (!is.null(copmat)) copmat <- reformcopmat(copmat, B, A)
+    if (!is.null(cparmat)) cparmat <- reformcopmat(cparmat, B, A)
+    newvars <- vars(rvine(B))
+    marg <- marg[sapply(ovars, function(v_) which(newvars == v_))]
+    rvine(B, copmat, cparmat, marg)
 }
 
-
+#' @export
+center <- function(...) UseMethod("center")
 
 #' Center a Vine Array
 #'
@@ -101,7 +136,8 @@ center.rvine <- function(rv) {
 #' a natural order array is outputted, since it satisfies that requirement).
 #' @return A vine array with the same dimensions as \code{A}.
 #' @export
-center.varray <- function(A) {
+centervarray <- function(A) {
+    warning("'centervarray()' is deprecated. Please use 'center.rvine()' instead.")
     ntrunc <- nrow(A) - 1
     d <- ncol(A)
     if (ntrunc == d-1) return(CopulaModel::varray2NO(A)$NOa)
