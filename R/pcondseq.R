fdbb5036259ce31d7eac4c2376553be85bd1b882
Vincenzo Coia <vincenzo.coia@gmail.com>
2016-09-06 16:10:50 -0700

    Added proper documentation to the pcondseq() function.

--- R/pcondseq.R
+++ R/pcondseq.R
@@ -1,15 +1,30 @@
-
-#' Find sequential predictors
+#' Compute Conditional Probabilities Sequentially
 #'
-#' For internal use.
+#' Given a vine describing the joint distribution of some uniform data, this
+#' function sequentially evaluates conditional distributions given previous
+#' variables in a specified order. See details.
 #'
 #' @param dat Data matrix with uniform scores.
 #' @param ord order of variables
-#' @param rv A regular vine, supposedly fitted to \code{dat}.
-#' @return Matrix with rows being observations, and columns being
-#' X[ord[1]]; X[ord[2]]|X[ord[1]]; X[ord[3]]|X[ord[1:2]]; etc.
-#' @note Expecting smart input: ord must be at least of length 1, with
-#' variables in the specified rvine.
+#' @param rv A regular vine, supposedly describing the joint
+#' distribution of \code{dat}.
+#' @details Suppose \code{Ui} denotes random variable \code{i}.
+#' If you specify \code{ord=c(5, 2, 3, 1)}, you'll get the cdfs of
+#' \code{U5}; \code{U2|U5}; \code{U3|(U2,U5)}; and \code{U1|(U3,U2,U5)},
+#' evaluated at the data.
+#'
+#' In general, this function computes:
+#'
+#' \itemize{
+#'      \item \code{U[ord[1]]}
+#'      \item \code{U[ord[2]]|U[ord[1]]}
+#'      \item \code{U[ord[3]]|U[ord[1:2]]}
+#'      \item ...
+#' }
+#'
+#' @return Matrix with rows being observations in \code{dat}, and columns being
+#' \code{U[ord[1]]}; \code{U[ord[2]]|U[ord[1]]};
+#' \code{U[ord[3]]|U[ord[1:2]]}; etc.
 #' @examples
 #' ## Make a vine
 #' G <- AtoG(CopulaModel::Dvinearray(4))
@@ -24,6 +39,9 @@
 #' pcondseq(dat, 1, rv)
 #' @export
 pcondseq <- function(dat, ord, rv) {
+    if (length(ord) == 0) return(matrix(ncol=0, nrow=nrow(dat)))
+    if (!all(ord %in% rv$G[1, ]))
+        stop("Some variables are not described by the 'rvine' object.")
     sapply(1:length(ord), function(i) {
         pcondrvine(dat, rv, var = ord[i], condset = ord[seq_len(i-1)])
     })
