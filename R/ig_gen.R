64e3a5e2220710e4a04a97b4170c4e5eb6a96026
Vincenzo Coia <vincenzo.coia@gmail.com>
2016-12-07 02:02:41 -0800

    Change the way that the IG generating function is inverted (now uses Newton-Raphson instead of interpolation). Update the documentation (still called igcond.lyx at the moment).

--- R/ig_gen.R
+++ R/ig_gen.R
@@ -37,48 +37,38 @@ ig_D1gen <- function(t, theta, k) {
 
 
 #' @param w Vector of values in [0,1] to evaluate the inverse function at.
-#' @param silent Logical; should the message output by \code{pcinterpolate()}
-#' be silenced?
 #' @rdname ig_gen
 #' @import CopulaModel
 #' @export
-ig_geninv <- function(w, theta, k, ngrid=1000, silent=TRUE) {
-    ## Main Idea: -----
-    ## Get values to evaluate ig_gen at, by using an approximation function
-    ##  that can be inverted. We'll invert that function on a grid in (0,1)
-    ##  to get a grid on the domain of H.
-    ## It seems to be easier to find an approximation function by transforming
-    ##  the domain from (1,oo) to (-oo,oo) by taking a log transform twice,
-    ##  so we'll do that.
-    ## ----------------
-    fun <- function(clean_w) {
-        ## Get grid on domain of H:
-        wmin <- min(0.001, min(clean_w)/2)
-        wmax <- max(0.999, (1+max(clean_w))/2)
-        wgrid <- seq(wmin, wmax, length.out=ngrid)
-        tgrid <- -log(-log(1-wgrid)) - log(1 + theta/k)
-        ## Evaluate H at that grid:
-        fn <- ig_gen(exp(exp(tgrid)), theta, k)
-        ## The evaluated ig_gen become the domain values of ig_geninv, and
-        ##  the tgrid values become the evaluated ig_geninv function.
-        ## Use the pcinterpolate function to evaluate at w.
-        der <- pcderiv(fn, tgrid)
-        ## Evaluate at the requested NA-free w's
-        if (silent) {
-            ## The only way I can find to silent a cat() call is to use sink,
-            ##  but send it to a non-existing file. Got answer from Stack
-            ##  Overflow by cbielow:
-            ## http://stackoverflow.com/questions/6177629/how-to-silence-the-output-from-this-r-package
-            f <- file()
-            sink(file=f)
-            res <- pcinterpolate(fn, tgrid, der, clean_w)[, 1]
-            sink()
-            close(f)
-        } else {
-            res <- pcinterpolate(fn, tgrid, der, clean_w)[, 1]
+ig_geninv <- function(p, eta, k, mxiter=40, eps=1.e-6, bd=5) {
+    ## Algorithm:
+    fun <- function(p, eta) {
+        ## Get starting values
+        xp1 <- 1/p
+        xp2 <- exp(1/eta/igl_geninv(p, k))
+        xpm <- pmin(xp1,xp2)
+        tt <- pmax(xpm - eps, 1 + (xpm-1)/2) # xpm-eps might overshoot left of 1.
+        iter <- 0
+        diff <- 1
+        ## Begin Newton-Raphson algorithm
+        while(iter<mxiter & max(abs(diff))>eps){
+            ## Helpful quantities
+            logt <- log(tt)
+            etalog <- eta * logt
+            etaloginv <- 1 / etalog
+            ## Evaluate functions
+            g <- tt*p - igl_gen(etaloginv, k)
+            gp <- p + etaloginv/tt/logt * igl_Dgen(etaloginv, k)
+            diff <- g/gp
+            tt <- tt-diff
+            while(max(abs(diff))>bd | any(tt<=1))
+            { diff <- diff/2; tt <- tt+diff }
+            iter <- iter+1
+            # cat(paste0("-----", iter, "-----\n"))
+            # cat(diff, "\n")
+            # cat(tt, "\n")
         }
-        ## Put back onto (1,oo) domain:
-        return(exp(exp(res)))
+        return(tt)
     }
-    eval_lims(fun, w, replx=c(0,1), replf=c(Inf, 1))
+    eval_lims(fun, p, replx=c(0, 1), replf=c(Inf, 1), eta=eta)
 }
